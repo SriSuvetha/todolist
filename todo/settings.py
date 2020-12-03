5b4b7d1f17d27f027fca83bd1e42585b5a454fe2
SRI SUVETHA C S <64887044+SriSuvetha@users.noreply.github.com>
2020-12-03 23:21:30 +0530

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q=$hbt#79pz$&)js60ur$zrbdw(d()5-)t8o-@)g-@y+hpc&h2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['todolist.herokuapp.com']
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'todo.urls'
